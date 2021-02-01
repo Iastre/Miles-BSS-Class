128983fff183cff87a29086a3c76e827819ca796
unknown <kfhankin@gmail.com>
2021-01-31 20:05:35 -0500

    Added file for homework 4

---
+++ Homework4_Queries.sql
@@ -0,0 +1,17 @@
+--Query to return users who have admin roles:
+
+--Query to return users with admin roles and information about their taverns:
+
+--All guests ordered by name (ascending) with classes and levels:
+
+--Top 10 sales in terms of sales price and what the services were:
+
+--Guests with 2 or more classes:
+
+--Guests with 2 or more classes with levels higher than 5:
+
+--Guests with only their highest level class:
+
+--Guests who stayed within a certain date range:
+
+--(Add IDENTITY and PRIMARY KEY constraints to the SELECT CREATE query)
